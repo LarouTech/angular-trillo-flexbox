b527070d6239699de958c7ff01a046e66e970af8
LarouTech <yanicklarouche@hotmail.com>
2020-01-12 19:22:43 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SidebarComponent } from './content/sidebar/sidebar.component';
+import { HotelViewComponent } from './content/hotel-view/hotel-view.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    AppComponent, HeaderComponent, ContentComponent, SidebarComponent, HotelViewComponent
   ],
   imports: [
     BrowserModule,
